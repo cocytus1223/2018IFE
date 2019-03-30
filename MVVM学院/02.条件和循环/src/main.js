ff7c49ba148f60a93e6b5b24fdf266559f1da6c3
Nocky <cocytus1223@gmail.com>
2019-03-30 23:30:44 +0800

    02.条件和循环

---
+++ MVVM学院/02.条件和循环/src/main.js
@@ -0,0 +1,9 @@
+import san from 'san';
+import {router} from 'san-router';
+import San from './app.san';
+
+console.log('hello webpack  San');
+
+router.add({rule: '/', Component: San, target: '#app'});
+
+router.start()
